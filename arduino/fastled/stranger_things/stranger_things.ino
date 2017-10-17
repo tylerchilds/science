abd72bc1a3aea10cd5c6b3d480839df68f7e591b
Tyler Childs <tylerwchilds@gmail.com>
2017-10-16 20:29:02 -0700

    neo pixel test and more stranger things

--- arduino/fastled/stranger_things/stranger_things.ino
+++ arduino/fastled/stranger_things/stranger_things.ino
@@ -1,64 +1,108 @@
 #include <FastLED.h>
 #define DATA_PIN 6  //this is the data pin connected to the LED strip.  If using WS2801 you also need a clock pin
-#define NUM_LEDS 50 //change this for the number of LEDs in the strip
+#define NUM_LEDS 100 //change this for the number of LEDs in the strip
 #define COLOR_ORDER RGB
+#define DELAY_SLOW 500
 #define DELAY 250
+#define DELAY_FAST 100
 
-#define RED 0xFF004D
-#define ORANGE 0xFFA300
-#define YELLOW 0xFFEC27
-#define GREEN 0x00E436
-#define BLUE 0x29ADFF
-#define INDIGO 0x83769C
-#define PINK 0xFF77A8
+#define RED 0x53000F
+#define YELLOW 0xD5A700
+#define GREEN 0x174C00
+#define BLUE 0x190894
+#define WHITE 0xEEEEEE
 #define OFF 0x000000
+
+#define HIGH_BRIGHTNESS 200
+#define LOW_BRIGHTNESS 120
 
 CRGB leds[NUM_LEDS]; 
 //I have a few additional integers in here from different tests. 
 
-long RAINBOW[7] = {BLUE, PINK, ORANGE, GREEN, YELLOW, RED, INDIGO};
+long RAINBOW[4] = {BLUE, GREEN, YELLOW, RED};
 
 String lookup[NUM_LEDS];
 
 void setup(){
-  Serial.begin(9600);
   FastLED.addLeds<WS2811, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS); //setting up the FastLED
 
-  FastLED.setBrightness(32);
-  lookup[0] = "a";
-  lookup[1] = "b";
-  lookup[2] = "c";
-  lookup[3] = "d";
-  lookup[5] = "e";
-  lookup[6] = "f";
-  lookup[7] = "g";
-  lookup[8] = "h";
-  lookup[11] = "i";
-  lookup[12] = "j";
-  lookup[13] = "k";
-  lookup[14] = "l";
-  lookup[15] = "m";
-  lookup[17] = "n";
-  lookup[18] = "o";
-  lookup[19] = "p";
-  lookup[20] = "q";
-  lookup[21] = "r";
-  lookup[22] = "s";
-  lookup[23] = "t";
-  lookup[24] = "u";
-  lookup[27] = "v";
-  lookup[28] = "w";
-  lookup[29] = "x";
-  lookup[30] = "y";
-  lookup[31] = "z";
+  FastLED.setBrightness(HIGH_BRIGHTNESS);
+  lookup[82] = "a";
+  lookup[80] = "b";
+  lookup[78] = "c";
+  lookup[75] = "d";
+  lookup[72] = "e";
+  lookup[69] = "f";
+  lookup[66] = "g";
+  lookup[62] = "h";
+  lookup[60] = "i";
+  lookup[57] = "j";
+  lookup[54] = "k";
+  lookup[51] = "l";
+  lookup[48] = "m";
+  lookup[1] = "n";
+  lookup[3] = "o";
+  lookup[6] = "p";
+  lookup[8] = "q";
+  lookup[11] = "r";
+  lookup[14] = "s";
+  lookup[17] = "t";
+  lookup[20] = "u";
+  lookup[22] = "v";
+  lookup[24] = "w";
+  lookup[27] = "x";
+  lookup[29] = "y";
+  lookup[33] = "z";
+  lookup[NUM_LEDS] = " ";
+
+  randomSeed(analogRead(0));
 }
 
 void loop(){
-  snake();
-  speak("abcdefghijklmnopqrstuvwxyz");
+  doSomething();
+  delay(random(4, 10) * 30000);
 }
 
+void doSomething(){
+  int range = random(100);
 
+
+  if(range < 45){
+    alert();
+  } else
+
+ 
+
+  if(range < 100){
+    pulse();
+  }
+}
+
+void sayAnything(){
+  int range = random(100);
+
+  if(range < 5){
+    speak("the quick brown fox jumped over the lazy dog");
+  } else
+  
+  if(range < 40){
+    speak("help me");
+  } else
+
+  if(range < 60){
+    speak("behind you");
+  } else
+
+  if(range < 80){
+    speak("upsidedown");
+  } else
+
+  if(range < 100){
+    speak("dont blink");
+  }
+
+ 
+}
 
 void on(){
   for (int i = 0; i < NUM_LEDS; i++) {
@@ -81,31 +125,35 @@ void off(){
 void blink(int led){
   leds[led] = colorAt(led);
   FastLED.show();
-  delay(DELAY);
+  delay(DELAY_SLOW);
   leds[led] = OFF;
   FastLED.show();
-  delay(DELAY);
+  delay(DELAY_SLOW);
 }
 
 void speak(String saying){
+  off();
   for(int i = 0; i < saying.length(); i++) {
     int led = indexFromLetter(saying.charAt(i));
     blink(led);
   }
+  on();
 }
 
 void alert(){
   int alertCount = 3;
   for (int n = 0; n < alertCount; n++) {
-    on();
     off();
+    on();
   }
 }
 
 void stripe(){
+  off();
+  
   int stripeDistance = 4;
   for (int n = 0; n < stripeDistance*3; n++) {
-    for (int i = 0; i < NUM_LEDS; i++) {
+    for (int i = NUM_LEDS; i >= 0 ; i--) {
       if((n+i) % stripeDistance == 0) {
         leds[i] = colorAt(i);
       } else {
@@ -113,17 +161,17 @@ void stripe(){
       }
     }
     FastLED.show();
-    delay(DELAY);
+    delay(DELAY_FAST);
   }
-
-  off();
 }
 
 
 void snake(){
+  off();
+  
   int distance = NUM_LEDS*3;
   
-  for (int i = 0; i < distance; i++) {
+  for (int i = NUM_LEDS; i >= 0; i--) {
     clear();
 
     int led0 = i % NUM_LEDS;
@@ -137,9 +185,28 @@ void snake(){
     leds[led3] = colorAt(led3);
 
     FastLED.show();
-    delay(25);
+    delay(10);
   }
-  off();
+}
+
+void pulse(){
+  on();
+  
+  int difference = HIGH_BRIGHTNESS - LOW_BRIGHTNESS;
+  int steps = 15;
+  int stepSize = difference / steps;
+
+  for (int i=0; i < steps; i++) {
+    FastLED.setBrightness(HIGH_BRIGHTNESS - (stepSize*i));
+    FastLED.show();
+    delay(20);
+  }
+
+  for (int i=0; i < steps; i++) {
+    FastLED.setBrightness(LOW_BRIGHTNESS + (stepSize*i));
+    FastLED.show();
+    delay(20);
+  }
 }
 
 long colorAt(int i){
@@ -147,7 +214,7 @@ long colorAt(int i){
 }
 
 int indexFromLetter(char letter){
-  int index = 49;
+  int index = NUM_LEDS;
   for (int i=0; i<NUM_LEDS; i++) {
     if (letter == lookup[i].charAt(0)) {
       index = i;
